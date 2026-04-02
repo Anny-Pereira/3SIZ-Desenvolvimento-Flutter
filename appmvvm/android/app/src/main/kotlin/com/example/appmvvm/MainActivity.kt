a3b6a6f0a180cbb6e91ab680480617c3f8ea5584
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-01 22:42:19 -0300

    App MVVM - Arquitetura de Projeto

---
+++ appmvvm/android/app/src/main/kotlin/com/example/appmvvm/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.appmvvm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ appmvvm/lib/data/mission_repository.dart
@@ -0,0 +1,18 @@
+import 'package:appmvvm/data/mission_remote_service.dart';
+import 'package:appmvvm/domain/mission_model.dart';
+
+abstract class MissionRepository {
+  Future<List<MissionModel>> getMissions();
+}
+
+class MissionRepositoryImpl implements MissionRepository{
+  final MissionRemoteService _service;
+
+  MissionRepositoryImpl(this._service);
+
+  @override
+  Future<List<MissionModel>> getMissions() {
+    return _service.getMissions();
+  }
+
+}
